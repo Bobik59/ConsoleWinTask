41f622edae7aed3f103328e1fa50801ccbbb484d
Bobik58 <loshininc@gmail.com>
2024-11-13 17:20:53 +0700

    Init

--- Task.cs
+++ Task.cs
@@ -4,89 +4,152 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
-
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ConsoleWinTask
 {
-    internal class Task
+    internal class Tasks
     {
         public class Task1 : IMenuItem
         {
-            public string Name => "Задание 1: Ожидание завершения процесса";
+            public string Name => "1. Вывод времение и даты";
 
             public void Execute()
             {
-                Console.Write("Введите имя запускаемого процесса: ");
-                string processName = Console.ReadLine();
+                var task1 = new Task(() => PrintDateTime("Start"));
+                task1.Start();
 
-                var process = Process.Start(processName);
-                process.WaitForExit();
+                var task2 = Task.Run(() => PrintDateTime("Run"));
 
-                Console.WriteLine($"Процесс завершен с кодом: {process.ExitCode}");
+                var task3 = Task.Factory.StartNew(() => PrintDateTime("Task.Factory.StartNew"));
+            }
+
+            static void PrintDateTime(string str)
+            {
+                Console.WriteLine($"Thread {str}: {DateTime.Now}");
             }
         }
         public class Task2 : IMenuItem
         {
-            public string Name => "Задание 2: Ожидание или принудительное завершение процесса";
+            public string Name => "2: простые числа до 1000";
 
-            public void Execute()
+            public async void Execute()
             {
-                Console.Write("Введите имя запускаемого процесса: ");
-                string processName = Console.ReadLine();
 
-                var process = System.Diagnostics.Process.Start(processName);
+                Console.WriteLine("Введите 1 число");
+                int a = Int32.Parse(Console.ReadLine());
 
-                Console.WriteLine("Нажмите 'W' для ожидания завершения или 'K' для завершения процесса.");
-                var key = Console.ReadKey(intercept: true).Key;
+                Console.WriteLine("Введите 2 число");
+                int b = Int32.Parse(Console.ReadLine());
 
-                if (key == ConsoleKey.W)
+                var task = new Task<List<int>>(() => addIsPrimeList(a, b));
+                var task1 = task.ContinueWith(t => PrintPrime(t.Result));
+
+                task.Start();
+
+
+                await task;
+                Console.WriteLine($"количество простых чисел: {task.Result.Count}");
+            }
+
+            public static bool IsPrime(int number)
+            {
+                for (int i = 2; i < number; i++)
                 {
-                    process.WaitForExit();
-                    Console.WriteLine($"\nПроцесс завершен с кодом: {process.ExitCode}");
+                    if (number % i == 0)
+                        return false;
                 }
-                else if (key == ConsoleKey.K)
+                return true;
+            }
+
+            static List<int> addIsPrimeList(int a, int b)
+            {
+                List<int> values = new List<int>();
+                for (int i = a; i <= b; i++)
                 {
-                    process.Kill();
-                    Console.WriteLine("\nПроцесс был завершен принудительно.");
+                    if (IsPrime(i))
+                    {
+                        values.Add(i);
+                    }
+                }
+
+                return values;
+            }
+
+            static void PrintPrime(List<int> values)
+            {
+                foreach (int i in values)
+                {
+                    Console.WriteLine(i);
                 }
             }
         }
 
         public class Task3 : IMenuItem
         {
-            public string Name => "Задание 3: Запуск процесса с аргументами для операции";
+
+            static List<int> ints = new List<int>();
+            public string Name => "3: вывод ■ Минимум ■ Максимум ■ Среднее ■ Сумму.";
 
             public void Execute()
             {
-                Console.Write("Введите имя запускаемого процесса: ");
-                string processName = Console.ReadLine();
-                Console.Write("Введите первое число: ");
-                string number1 = Console.ReadLine();
-                Console.Write("Введите второе число: ");
-                string number2 = Console.ReadLine();
-                Console.Write("Введите операцию (+, -, *, /): ");
-                string operation = Console.ReadLine();
+                CreateList();
 
-                var process = System.Diagnostics.Process.Start(processName, $"{number1} {number2} {operation}");
-                process.WaitForExit();
+                Task[] tasks =
+                {
+                    new Task(FindMin),
+                    new Task(FindMax),
+                    new Task(FindAverage),
+                    new Task(FindSum),
+
+                };
+
+                foreach (var item in tasks)
+                    item.Start();
             }
+
+            static void FindMin()
+            {
+                Console.WriteLine($"Min: {ints.Min()}");
+            }
+            static void FindMax()
+            {
+                Console.WriteLine($"Max: {ints.Max()}");
+            }
+
+            static void FindAverage()
+            {
+                Console.WriteLine($"Avg: {ints.Average()}");
+            }
+
+            static void FindSum()
+            {
+                Console.WriteLine($"Sum: {ints.Sum()}");
+            }
+
+            static void CreateList()
+            {
+                Console.Write("Введите целое число больше нуля: ");
+                int num = int.Parse(Console.ReadLine());
+
+                Random rnd = new Random();
+                for (int i = 0; i < num; i++)
+                    ints.Add(rnd.Next(0, 100));
+            }
+
         }
 
         public class Task4 : IMenuItem
         {
-            public string Name => "Задание 4: Поиск слова в файле";
+            public string Name => "4. работа с массивом";
 
             public void Execute()
             {
-                Console.Write("Введите имя запускаемого процесса: ");
-                string processName = Console.ReadLine();
-                Console.Write("Введите путь к файлу: ");
-                string filePath = Console.ReadLine();
-                Console.Write("Введите слово для поиска: ");
-                string word = Console.ReadLine();
+                Console.WriteLine("Введите размер массива: ");
+                int num = int.Parse(Console.ReadLine());
+                int[] mass = new int[num];
+                int[] result = array.Distinct().ToArray();
 
-                var process = System.Diagnostics.Process.Start(processName, $"{filePath} {word}");
-                process.WaitForExit();
             }
         }
     }
